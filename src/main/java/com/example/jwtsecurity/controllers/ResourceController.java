41cfc15ba9dcd0ede2c8fb4dab48448dde90a422
Dechanel DIPEUA <dechaneldipeua@gmail.com>
2024-06-02 16:01:12 +0100

    init commit

---
+++ src/main/java/com/example/jwtsecurity/controllers/ResourceController.java
@@ -0,0 +1,15 @@
+package com.example.jwtsecurity.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+    @GetMapping("/")
+    public String getResource(){
+        return  "a value...";
+    }
+
+}
